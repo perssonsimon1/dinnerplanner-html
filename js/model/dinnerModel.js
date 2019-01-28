34514ffae1795743583693054f665845f51525d2
Simon Persson <simon@akep.se>
2019-01-28 12:28:00 +0100

    Update dinnerModel.js

--- js/model/dinnerModel.js
+++ js/model/dinnerModel.js
@@ -1,43 +1,47 @@
 //DinnerModel Object constructor
-var DinnerModel = function () {
-	var numberOfGuests = 3;
-	var menu = [];
+class DinnerModel {
+	let numberOfGuests = 3;
+	const menu = [];
+
+	constructor() {
+		this.dishes = dishesConst;
+	}
 
 	//TODO Lab 1 implement the data structure that will hold number of guest
 	// and selected dishes for the dinner menu
 
-	this.setNumberOfGuests = function (num) {
+	setNumberOfGuests(num) {
 		if (num >= 0) numberOfGuests = num;
 	}
 
-	this.getNumberOfGuests = function () {
+	getNumberOfGuests() {
 		return numberOfGuests;
 	}
 
 	//Returns the dish that is on the menu for selected type 
-	this.getSelectedDish = function (type) {
+	getSelectedDish(type) {
 		var theDish = menu.find(dish => dish.type = type);
 		return theDish;
 	}
 
 	//Returns all the dishes on the menu.
-	this.getFullMenu = function () {
+	getFullMenu() {
 		return menu;
 	}
 
-	this.getDishes = function () {
+	getDishes() {
 		return dishes;
 	}
 
 
 	//Returns all ingredients for all the dishes on the menu.
-	this.getAllIngredients = function () {
+	getAllIngredients() {
 		return menu
 			.flatMap(dish => dish.ingredients);
 	}
 
 	//Returns the total price of the menu (all the ingredients multiplied by number of guests).
-	this.getTotalMenuPrice = function () {
+	getTotalMenuPrice() {
 		return menu
 			.flatMap(dish => dish.ingredients)
 			.reduce((acc, val) => acc + val.price * numberOfGuests, 0);
@@ -45,7 +49,7 @@ var DinnerModel = function () {
 
 	//Adds the passed dish to the menu. If the dish of that type already exists on the menu
 	//it is removed from the menu and the new one added.
-	this.addDishToMenu = function (id) {
+	addDishToMenu(id) {
 		var existingDishes = menu.find(dish => dish.type == this.getDish(id).type);
 		if (existingDishes != undefined) {
 			this.removeDishFromMenu(existingDishes.id);
@@ -56,7 +60,7 @@ var DinnerModel = function () {
 	}
 
 	//Removes dish from menu
-	this.removeDishFromMenu = function (id) {
+	removeDishFromMenu(id) {
 		var index = menu.findIndex(dish => dish.id == id);
 		if (index != -1) menu.splice(index,1);
 	}
@@ -65,7 +69,7 @@ var DinnerModel = function () {
 	//function that returns all dishes of specific type (i.e. "starter", "main dish" or "dessert")
 	//you can use the filter argument to filter out the dish by name or ingredient (use for search)
 	//if you don't pass any filter all the dishes will be returned
-	this.getAllDishes = function (type, filter) {
+	getAllDishes(type, filter) {
 		return dishes.filter(function (dish) {
 			var found = true;
 			if (filter) {
@@ -84,7 +88,7 @@ var DinnerModel = function () {
 	}
 
 	//function that returns a dish of specific ID
-	this.getDish = function (id) {
+	getDish(id) {
 		for (key in dishes) {
 			if (dishes[key].id == id) {
 				return dishes[key];
@@ -92,6 +96,7 @@ var DinnerModel = function () {
     }
 	  return undefined;
   }
+}
 
 	// the dishes constant contains an array of all the 
 	// dishes in the database. Each dish has id, name, type,
@@ -101,246 +106,245 @@ var DinnerModel = function () {
 	// defining the unit i.e. "g", "slices", "ml". Unit
 	// can sometimes be empty like in the example of eggs where
 	// you just say "5 eggs" and not "5 pieces of eggs" or anything else.
-	var dishes = [{
-		'id': 1,
-		'name': 'French toast',
-		'type': 'starter',
-		'image': 'toast.jpg',
-		'description': "In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
-		'ingredients': [{
-			'name': 'eggs',
-			'quantity': 0.5,
-			'unit': '',
-			'price': 10
-		}, {
-			'name': 'milk',
-			'quantity': 30,
-			'unit': 'ml',
-			'price': 6
-		}, {
-			'name': 'brown sugar',
-			'quantity': 7,
-			'unit': 'g',
-			'price': 1
-		}, {
-			'name': 'ground nutmeg',
-			'quantity': 0.5,
-			'unit': 'g',
-			'price': 12
-		}, {
-			'name': 'white bread',
-			'quantity': 2,
-			'unit': 'slices',
-			'price': 2
-		}]
-	}, {
-		'id': 2,
-		'name': 'Sourdough Starter',
-		'type': 'starter',
-		'image': 'sourdough.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'active dry yeast',
-			'quantity': 0.5,
-			'unit': 'g',
-			'price': 4
-		}, {
-			'name': 'warm water',
-			'quantity': 30,
-			'unit': 'ml',
-			'price': 0
-		}, {
-			'name': 'all-purpose flour',
-			'quantity': 15,
-			'unit': 'g',
-			'price': 2
-		}]
-	}, {
-		'id': 3,
-		'name': 'Baked Brie with Peaches',
-		'type': 'starter',
-		'image': 'bakedbrie.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'round Brie cheese',
-			'quantity': 10,
-			'unit': 'g',
-			'price': 8
-		}, {
-			'name': 'raspberry preserves',
-			'quantity': 15,
-			'unit': 'g',
-			'price': 10
-		}, {
-			'name': 'peaches',
-			'quantity': 1,
-			'unit': '',
-			'price': 4
-		}]
-	}, {
-		'id': 100,
-		'name': 'Meat balls',
-		'type': 'main dish',
-		'image': 'meatballs.jpg',
-		'description': "Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
-		'ingredients': [{
-			'name': 'extra lean ground beef',
-			'quantity': 115,
-			'unit': 'g',
-			'price': 20
-		}, {
-			'name': 'sea salt',
-			'quantity': 0.7,
-			'unit': 'g',
-			'price': 3
-		}, {
-			'name': 'small onion, diced',
-			'quantity': 0.25,
-			'unit': '',
-			'price': 2
-		}, {
-			'name': 'garlic salt',
-			'quantity': 0.7,
-			'unit': 'g',
-			'price': 2
-		}, {
-			'name': 'Italian seasoning',
-			'quantity': 0.6,
-			'unit': 'g',
-			'price': 3
-		}, {
-			'name': 'dried oregano',
-			'quantity': 0.3,
-			'unit': 'g',
-			'price': 3
-		}, {
-			'name': 'crushed red pepper flakes',
-			'quantity': 0.6,
-			'unit': 'g',
-			'price': 3
-		}, {
-			'name': 'Worcestershire sauce',
-			'quantity': 6,
-			'unit': 'ml',
-			'price': 7
-		}, {
-			'name': 'milk',
-			'quantity': 20,
-			'unit': 'ml',
-			'price': 4
-		}, {
-			'name': 'grated Parmesan cheese',
-			'quantity': 5,
-			'unit': 'g',
-			'price': 8
-		}, {
-			'name': 'seasoned bread crumbs',
-			'quantity': 15,
-			'unit': 'g',
-			'price': 4
-		}]
-	}, {
-		'id': 101,
-		'name': 'MD 2',
-		'type': 'main dish',
-		'image': 'bakedbrie.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ingredient 1',
-			'quantity': 1,
-			'unit': 'pieces',
-			'price': 8
-		}, {
-			'name': 'ingredient 2',
-			'quantity': 15,
-			'unit': 'g',
-			'price': 7
-		}, {
-			'name': 'ingredient 3',
-			'quantity': 10,
-			'unit': 'ml',
-			'price': 4
-		}]
-	}, {
-		'id': 102,
-		'name': 'MD 3',
-		'type': 'main dish',
-		'image': 'meatballs.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ingredient 1',
-			'quantity': 2,
-			'unit': 'pieces',
-			'price': 8
-		}, {
-			'name': 'ingredient 2',
-			'quantity': 10,
-			'unit': 'g',
-			'price': 7
-		}, {
-			'name': 'ingredient 3',
-			'quantity': 5,
-			'unit': 'ml',
-			'price': 4
-		}]
-	}, {
-		'id': 103,
-		'name': 'MD 4',
-		'type': 'main dish',
-		'image': 'meatballs.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ingredient 1',
-			'quantity': 1,
-			'unit': 'pieces',
-			'price': 4
-		}, {
-			'name': 'ingredient 2',
-			'quantity': 12,
-			'unit': 'g',
-			'price': 7
-		}, {
-			'name': 'ingredient 3',
-			'quantity': 6,
-			'unit': 'ml',
-			'price': 4
-		}]
-	}, {
-		'id': 200,
-		'name': 'Chocolat Ice cream',
-		'type': 'dessert',
-		'image': 'icecream.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ice cream',
-			'quantity': 100,
-			'unit': 'ml',
-			'price': 6
-		}]
-	}, {
-		'id': 201,
-		'name': 'Vanilla Ice cream',
-		'type': 'dessert',
-		'image': 'icecream.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ice cream',
-			'quantity': 100,
-			'unit': 'ml',
-			'price': 6
-		}]
-	}, {
-		'id': 202,
-		'name': 'Strawberry',
-		'type': 'dessert',
-		'image': 'icecream.jpg',
-		'description': "Here is how you make it... Lore ipsum...",
-		'ingredients': [{
-			'name': 'ice cream',
-			'quantity': 100,
-			'unit': 'ml',
-			'price': 6
-		}]
-	}];
-
-}
+	const dishesConst = [{
+		'id':1,
+		'name':'French toast',
+		'type':'starter',
+		'image':'toast.jpg',
+		'description':"In a large mixing bowl, beat the eggs. Add the milk, brown sugar and nutmeg; stir well to combine. Soak bread slices in the egg mixture until saturated. Heat a lightly oiled griddle or frying pan over medium high heat. Brown slices on both sides, sprinkle with cinnamon and serve hot.",
+		'ingredients':[{ 
+			'name':'eggs',
+			'quantity':0.5,
+			'unit':'',
+			'price':10
+			},{
+			'name':'milk',
+			'quantity':30,
+			'unit':'ml',
+			'price':6
+			},{
+			'name':'brown sugar',
+			'quantity':7,
+			'unit':'g',
+			'price':1
+			},{
+			'name':'ground nutmeg',
+			'quantity':0.5,
+			'unit':'g',
+			'price':12
+			},{
+			'name':'white bread',
+			'quantity':2,
+			'unit':'slices',
+			'price':2
+			}]
+		},{
+		'id':2,
+		'name':'Sourdough Starter',
+		'type':'starter',
+		'image':'sourdough.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'active dry yeast',
+			'quantity':0.5,
+			'unit':'g',
+			'price':4
+			},{
+			'name':'warm water',
+			'quantity':30,
+			'unit':'ml',
+			'price':0
+			},{
+			'name':'all-purpose flour',
+			'quantity':15,
+			'unit':'g',
+			'price':2
+			}]
+		},{
+		'id':3,
+		'name':'Baked Brie with Peaches',
+		'type':'starter',
+		'image':'bakedbrie.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'round Brie cheese',
+			'quantity':10,
+			'unit':'g',
+			'price':8
+			},{
+			'name':'raspberry preserves',
+			'quantity':15,
+			'unit':'g',
+			'price':10
+			},{
+			'name':'peaches',
+			'quantity':1,
+			'unit':'',
+			'price':4
+			}]
+		},{
+		'id':100,
+		'name':'Meat balls',
+		'type':'main dish',
+		'image':'meatballs.jpg',
+		'description':"Preheat an oven to 400 degrees F (200 degrees C). Place the beef into a mixing bowl, and season with salt, onion, garlic salt, Italian seasoning, oregano, red pepper flakes, hot pepper sauce, and Worcestershire sauce; mix well. Add the milk, Parmesan cheese, and bread crumbs. Mix until evenly blended, then form into 1 1/2-inch meatballs, and place onto a baking sheet. Bake in the preheated oven until no longer pink in the center, 20 to 25 minutes.",
+		'ingredients':[{ 
+			'name':'extra lean ground beef',
+			'quantity':115,
+			'unit':'g',
+			'price':20
+			},{
+			'name':'sea salt',
+			'quantity':0.7,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'small onion, diced',
+			'quantity':0.25,
+			'unit':'',
+			'price':2
+			},{
+			'name':'garlic salt',
+			'quantity':0.7,
+			'unit':'g',
+			'price':2
+			},{
+			'name':'Italian seasoning',
+			'quantity':0.6,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'dried oregano',
+			'quantity':0.3,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'crushed red pepper flakes',
+			'quantity':0.6,
+			'unit':'g',
+			'price':3
+			},{
+			'name':'Worcestershire sauce',
+			'quantity':6,
+			'unit':'ml',
+			'price':7
+			},{
+			'name':'milk',
+			'quantity':20,
+			'unit':'ml',
+			'price':4
+			},{
+			'name':'grated Parmesan cheese',
+			'quantity':5,
+			'unit':'g',
+			'price':8
+			},{
+			'name':'seasoned bread crumbs',
+			'quantity':15,
+			'unit':'g',
+			'price':4
+			}]
+		},{
+		'id':101,
+		'name':'MD 2',
+		'type':'main dish',
+		'image':'bakedbrie.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ingredient 1',
+			'quantity':1,
+			'unit':'pieces',
+			'price':8
+			},{
+			'name':'ingredient 2',
+			'quantity':15,
+			'unit':'g',
+			'price':7
+			},{
+			'name':'ingredient 3',
+			'quantity':10,
+			'unit':'ml',
+			'price':4
+			}]
+		},{
+		'id':102,
+		'name':'MD 3',
+		'type':'main dish',
+		'image':'meatballs.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ingredient 1',
+			'quantity':2,
+			'unit':'pieces',
+			'price':8
+			},{
+			'name':'ingredient 2',
+			'quantity':10,
+			'unit':'g',
+			'price':7
+			},{
+			'name':'ingredient 3',
+			'quantity':5,
+			'unit':'ml',
+			'price':4
+			}]
+		},{
+		'id':103,
+		'name':'MD 4',
+		'type':'main dish',
+		'image':'meatballs.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ingredient 1',
+			'quantity':1,
+			'unit':'pieces',
+			'price':4
+			},{
+			'name':'ingredient 2',
+			'quantity':12,
+			'unit':'g',
+			'price':7
+			},{
+			'name':'ingredient 3',
+			'quantity':6,
+			'unit':'ml',
+			'price':4
+			}]
+		},{
+		'id':200,
+		'name':'Chocolat Ice cream',
+		'type':'dessert',
+		'image':'icecream.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ice cream',
+			'quantity':100,
+			'unit':'ml',
+			'price':6
+			}]
+		},{
+		'id':201,
+		'name':'Vanilla Ice cream',
+		'type':'dessert',
+		'image':'icecream.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ice cream',
+			'quantity':100,
+			'unit':'ml',
+			'price':6
+			}]
+		},{
+		'id':202,
+		'name':'Strawberry',
+		'type':'dessert',
+		'image':'icecream.jpg',
+		'description':"Here is how you make it... Lore ipsum...",
+		'ingredients':[{ 
+			'name':'ice cream',
+			'quantity':100,
+			'unit':'ml',
+			'price':6
+			}]
+		}
+];
