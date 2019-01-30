8aaca3253001d5aedb9892c8c74d9adf7ab253f8
Oskar <oskar.norinder@gmail.com>
2019-01-30 19:47:30 +0100

    dishItemView uppdaterare inte

--- js/controller/dishSearchController.js
+++ js/controller/dishSearchController.js
@@ -2,9 +2,10 @@ class DishSearchController {
     constructor(gsc, view,otherView, model) {
 
 
-        view.searchBtn.addEventListener('click', event => 
-        otherView.render(view.optionsMenu.slectedOptions.value,view.filterInput.value)
-        );
+        view.searchBtn.addEventListener('click', event => {
+        //console.log(view.optionsMenu.selectedOptions,view.filterInput.value);
+        otherView.render(view.optionsMenu.selectedOptions[0].value,view.filterInput.value);
+                }        );
 
     }
 
