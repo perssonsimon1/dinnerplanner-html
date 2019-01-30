9143ef81a53d34f786a969b38e9be2201c11fe59
Simon Persson <simon@akep.se>
2019-01-30 13:59:25 +0100

    Updated goBackView

--- js/view/goBackView.js
+++ js/view/goBackView.js
@@ -6,24 +6,12 @@ class GoBackView {
     }
 
     render() {
-        const dinnerText = document.createElement('h3');
+        const dinnerText = this.container.querySelector('#dinnerText');
         dinnerText.innerHTML = "My dinner for " + this.model.getNumberOfGuests() + ' people';
-        const button = document.createElement('button');
-        button.innerHTML = "Go back and edit dinner";
-        button.classList.add('float-right', 'btn', 'btn-light', 'searchView-btn');
-        button.id = "goBack";
         this.container.appendChild(dinnerText);
-        this.container.appendChild(button);
-    }
-
-    clear() {
-        while (this.container.firstChild) {
-            this.container.removeChild(this.container.firstChild);
-        }
     }
 
     update() {
-        this.clear();
         this.render();
     }
 }
