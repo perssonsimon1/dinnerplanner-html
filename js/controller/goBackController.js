cdec05d72af766bf4ebe40437eddf66260484dbc
onorin <onorin@kth.se>
2019-01-30 13:32:30 +0100

    sidebar update

--- js/controller/goBackController.js
+++ js/controller/goBackController.js
@@ -1,5 +1,5 @@
 class GoBackController {
     constructor(gsc, view, model) {
-        view.querySelector('#go-back').addEventListener('click', event => gsc.showScreen('DISH_SEARCH'));
+        view.querySelector('#goBack').addEventListener('click', event => gsc.showScreen('DISH_SEARCH'));
     }
 }
--- js/controller/sidebarController.js
+++ js/controller/sidebarController.js
@@ -1,5 +1,7 @@
 class SidebarController {
     constructor(gsc, view, model) {
         view.querySelector('#confirmDinner').addEventListener('click', event => gsc.showScreen('DINNER_OVERVIEW'));
+        var inputer = view.querySelector("#people").addEventListener('input', event => model.setNumberOfGuests(event.target.value));
     }
+
 }
