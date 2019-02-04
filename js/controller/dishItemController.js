551c973a1ac64192e8495fc027ee34a33dc23fd4
Simon Persson <simon@akep.se>
2019-02-04 11:31:52 +0100

    Removed unused logging message

--- js/controller/dishItemController.js
+++ js/controller/dishItemController.js
@@ -2,9 +2,8 @@ class DishItemController {
     constructor(gsc, view, model) {
         view.rows.addEventListener('click', event =>{
             if (event.target.parentElement.matches ('.dish-item')){
-            model.setCurrentDish(event.target.parentElement.getAttribute('dishID'));
-            console.log(event.target.parentElement.getAttribute('dishID'),"ble");
-             gsc.showScreen('DISH_DETAILS');
+                model.setCurrentDish(event.target.parentElement.getAttribute('dishID'));
+                gsc.showScreen('DISH_DETAILS');
             }
         });
     }
